71d5a1d2e79fcbbb14f0a6d85a2a7658a2f7deb4
Sangeetha-p-in <60625081+Sangeetha-p-in@users.noreply.github.com>
2020-03-10 14:03:30 +0530

    Elastic

---
+++ StarWarApi/LambdaFunction.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+using Amazon.Lambda.AspNetCoreServer;
+using Microsoft.AspNetCore.Hosting;
+
+namespace StarWarApi
+{
+    public class LambdaFunction : APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder.UseApiGateway();
+        }
+    }
+}
